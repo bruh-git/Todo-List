f7c8367f9df19fe2d0d181062d3d035a872072c5
Bruna <brunac.cardososilva@gmail.com>
2021-12-21 14:49:25 -0300

    requesitos

---
+++ script.js
@@ -0,0 +1,17 @@
+var buttonCriarTarefa = document.getElementById('criar-tarefa')
+
+
+buttonCriarTarefa.addEventListener('click', add);
+
+function add(){
+    var li = document.createElement('li');
+    
+    li.innerText = input.value;
+
+}
+
+li.addEventListener('click',grayBackground);
+
+function grayBackground (){
+    document.querySelectorAll('')
+}
